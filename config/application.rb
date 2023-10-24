7d9853875d0d801b04d9c84be6233b82d54541da
Jaime <yonhaime@gmail.com>
2023-10-23 22:19:22 -0400

    offenses

--- config/application.rb
+++ config/application.rb
@@ -1,15 +1,15 @@
-require_relative "boot"
-require "rails"
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "active_storage/engine"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_mailbox/engine"
-require "action_text/engine"
-require "action_view/railtie"
-require "action_cable/engine"
+require_relative 'boot'
+require 'rails'
+require 'active_model/railtie'
+require 'active_job/railtie'
+require 'active_record/railtie'
+require 'active_storage/engine'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'action_mailbox/engine'
+require 'action_text/engine'
+require 'action_view/railtie'
+require 'action_cable/engine'
 
 Bundler.require(*Rails.groups)
 
@@ -22,6 +22,6 @@ module Templarius
     config.view_component.default_preview_layout = 'lookbook'
     config.view_component.preview_paths << Rails.root.join('app/components').to_s
     config.paths.add 'app/components', eager_load: true, glob: '*'
-    config.autoload_lib(ignore: %w(assets tasks))
+    config.autoload_lib(ignore: %w[assets tasks])
   end
 end
